6cd96a544539b83ecc3f4c42465dd729fbb2549c
khanhpd <duykhanh141197@gmail.com>
2025-03-25 16:10:09 +0700

    remove files

--- netlify/functions/authentication/user-authentication.js
+++
@@ -1,8 +0,0 @@
-import initSupabase  from './init-supabase.js';
-
-async function signIn(email, password) {
-    const supabase = initSupabase();
-    const { user, error } = await supabase.auth.signInWithPassword({ email, password });
-    if (error) console.error("Login failed:", error);
-    else console.log("User logged in:", user);
-}
