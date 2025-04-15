576bd9989d5229e5ae1b9bcadc3de0584b5d951e
khanhpd <duykhanh141197@gmail.com>
2025-04-15 13:22:47 +0700

    config bien moi truong o local cho resource page

--- src/pages/resource/fetch-data.js
+++ src/pages/resource/fetch-data.js
@@ -9,7 +9,7 @@ export async function fetchDataGraphApi() {
 
         // http://127.0.0.1:54321/functions/v1/get-pages-data
         // https://pmybhyeyienzwgthbfkh.supabase.co/functions/v1/get-pages-data
-        const response = await fetch('https://pmybhyeyienzwgthbfkh.supabase.co/functions/v1/get-pages-data', {
+        const response = await fetch('http://127.0.0.1:54321/functions/v1/get-pages-data', {
             method: "GET",
             headers: {
                 'Content-Type': 'application/json',
--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -3,7 +3,7 @@
  * https://platform.omegaa.cloud/
  */
 export const corsHeaders = {
-    'Access-Control-Allow-Origin': 'https://platform.omegaa.cloud/',
+    'Access-Control-Allow-Origin': 'https://localhost:3000',
     'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
     'Content-Type': 'application/json',
